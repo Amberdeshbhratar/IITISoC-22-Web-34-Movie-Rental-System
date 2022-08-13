2cb2b60f193d6025338ef3be1f76e11168cd6bae
vishesh <visheshgargbnl@gmail.com>
2022-08-13 22:42:18 +0530

    updated contact

--- iitipro/urls.py
+++ iitipro/urls.py
@@ -9,5 +9,5 @@ path('register', views.register ,name='register'),
 path('login',views.login , name='login'),
 path('logout',views.logout , name='logout'),
 path('contactus' , views.contactus , name='contactus'),
-path('about',views.about , name='about')
+path('about',views.about , name='about'),
 ]
